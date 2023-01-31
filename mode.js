4faf684ac734548ebf67b64cb2d23f97755b2080
AlexanderApollGray <90724200+AlexanderApollGray@users.noreply.github.com>
2023-01-31 11:15:46 -0700

    Update mode.js

--- mode.js
+++ mode.js
@@ -4,12 +4,24 @@
 
 // Setup mode #1
 // Setup canvas
-let cnv = document.getElementById("timedCanvas");
-let ctx = cnv.getContext("2d");
-cnv.width = 150;
-cnv.height = 100;
+let cnv3 = document.getElementById("mode1Canvas");
+let ctx3 = cnv3.getContext("2d");
+cnv3.width = 175;
+cnv3.height = 125;
 
 // Draw text
-ctx.font = "30px Arial, Helvetica, sans-serif";
-ctx.fillStyle = "#8B008B";
-ctx.fillText("Play", 45, 60);
+ctx3.font = "30px Arial, Helvetica, sans-serif";
+ctx3.fillStyle = "#8B008B";
+ctx3.fillText("Timed", 45, 72.5);
+
+// Setup mode #2
+// Setup canvas
+let cnv4 = document.getElementById("mode2Canvas");
+let ctx4 = cnv4.getContext("2d");
+cnv4.width = 175;
+cnv4.height = 125;
+
+// Draw text
+ctx4.font = "30px Arial, Helvetica, sans-serif";
+ctx4.fillStyle = "#8B008B";
+ctx4.fillText("Speedrun", 20, 72.5);
